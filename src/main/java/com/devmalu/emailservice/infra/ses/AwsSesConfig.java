daba9390ca4bb8ed627f8c5c5d8c8e7b323371d2
Malú <130713333+malummartiins@users.noreply.github.com>
2023-10-17 18:57:46 -0300

    Creat config AWS

---
+++ src/main/java/com/devmalu/emailservice/infra/ses/AwsSesConfig.java
@@ -0,0 +1,17 @@
+package com.devmalu.emailservice.infra.ses;
+
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService(){
+        return AmazonSimpleEmailServiceClientBuilder.standard().build();
+    }
+
+
+}
