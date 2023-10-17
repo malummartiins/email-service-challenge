76a1e82d49dbfc03f15583495bb6dff7c0b4582f
Malú <130713333+malummartiins@users.noreply.github.com>
2023-10-17 18:37:22 -0300

    create controller class

---
+++ src/main/java/com/devmalu/emailservice/controllers/EmailSendersController.java
@@ -0,0 +1,10 @@
+package com.devmalu.emailservice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSendersController {
+    
+}
